1f8a1848ce0af3b1f5a850ca40e6be637d129169
hui <a3287134551@gmail.com>
2024-06-05 23:56:10 +0800

    更改了界面的显示，欠缺对状态的判断

--- apple_网单_改地址/Change_the_address.py
+++ apple_网单_改地址/Change_the_address.py
@@ -51,16 +51,14 @@ class Asynctask(QRunnable):
             result = {"firstName": "账号异常"}
         else:
             result = {
-                "firstName": apple.firstName,
-                "lastName": apple.lastName,
-                "companyName": apple.companyName,
-                "street": apple.street,
-                "street2": apple.street2,
-                "postalCode": apple.postalCode,
                 "city": apple.city,
                 "state": apple.state,
-                "countryCode": apple.countryCode,
-                "fullDaytimePhone": apple.fullDaytimePhone,
+                "lastName": apple.lastName,
+                "firstName": apple.firstName,
+                "companyName": apple.companyName,
+                "street": apple.street,
+                "postalCode": apple.postalCode,
+                "street2": apple.street2,
             }
         return result
 
@@ -70,44 +68,38 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Change_the_address")
-        self.setGeometry(300, 300, 1200, 600)
+        self.setGeometry(300, 300, 1150, 600)
 
         self.thread_pool = QThreadPool()
 
         main_layout = QVBoxLayout()
         button_layout = QHBoxLayout()
-        label_layout = QHBoxLayout()
         form_layout = QHBoxLayout()
         table_layout = QHBoxLayout()
         main_layout.addLayout(button_layout)
-        main_layout.addLayout(label_layout)
         main_layout.addLayout(form_layout)
         main_layout.addLayout(table_layout)
         button_layout.setAlignment(Qt.AlignLeft)
-        label_layout.setAlignment(Qt.AlignLeft)
         form_layout.setAlignment(Qt.AlignLeft)
         table_layout.setAlignment(Qt.AlignLeft)
         main_layout.setAlignment(Qt.AlignTop)
 
         self.tableWidget = QTableWidget()
-        self.tableWidget.setColumnCount(12)
-        # self.tableWidget.setFixedWidth(1200)
-        # self.tableWidget.setFixedHeight(500)
+        self.tableWidget.setColumnCount(11)
         table_layout.addWidget(self.tableWidget)
         self.tableWidget.setHorizontalHeaderLabels(
             [
-                "邮箱",
+                "账号",
                 "密码",
-                "名",
-                "姓",
-                "公司名称",
-                "街道地址",
-                "公寓",
-                "邮政编码",
+                "链接",
                 "城市",
                 "州",
-                "国家地区",
-                "电话号码",
+                "姓",
+                "名",
+                "公司名称",
+                "街道",
+                "邮政编码",
+                "街道2",
             ]
         )
 
@@ -130,99 +122,32 @@ class MainWindow(QMainWindow):
         self.button5.clicked.connect(self.on_click_button5)
         self.button6.clicked.connect(self.on_click_button6)
 
-        self.label1 = QLabel("名")
-        self.label1.setFixedWidth(100)
-        label_layout.addWidget(self.label1)
         self.comboBox1 = QComboBox()
         self.comboBox1.setFixedWidth(1000)
         form_layout.addWidget(self.comboBox1)
-        self.comboBox1.setStyleSheet("QComboBox { border: 1px solid lightgray; background-color: black; } "
-                             "QComboBox QAbstractItemView { border: 0; } "
-                             "QComboBox QAbstractItemView::item { border-bottom: 1px solid lightgray; }")
-
-
-
-        self.label2 = QLabel("姓")
-        self.label2.setFixedWidth(100)
-        label_layout.addWidget(self.label2)
-        # self.comboBox2 = QComboBox()
-        # self.comboBox2.setFixedWidth(100)
-        # self.comboBox2.addItem("选项1", "值1")
-        # self.comboBox2.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox2)
-
-        self.label3 = QLabel("公司名称")
-        self.label3.setFixedWidth(100)
-        label_layout.addWidget(self.label3)
-        # self.comboBox3 = QComboBox()
-        # self.comboBox3.setFixedWidth(100)
-        # self.comboBox3.addItem("选项1", "值1")
-        # self.comboBox3.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox3)
-
-        self.label4 = QLabel("街道地址")
-        self.label4.setFixedWidth(100)
-        label_layout.addWidget(self.label4)
-        # self.comboBox4 = QComboBox()
-        # self.comboBox4.setFixedWidth(100)
-        # self.comboBox4.addItem("选项1", "值1")
-        # self.comboBox4.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox4)
-
-        self.label5 = QLabel("公寓")
-        self.label5.setFixedWidth(100)
-        label_layout.addWidget(self.label5)
-        # self.comboBox5 = QComboBox()
-        # self.comboBox5.setFixedWidth(100)
-        # self.comboBox5.addItem("选项1", "值1")
-        # self.comboBox5.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox5)
-
-        self.label6 = QLabel("邮政编码")
-        self.label6.setFixedWidth(100)
-        label_layout.addWidget(self.label6)
-        # self.comboBox6 = QComboBox()
-        # self.comboBox6.setFixedWidth(100)
-        # self.comboBox6.addItem("选项1", "值1")
-        # self.comboBox6.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox6)
-
-        self.label7 = QLabel("城市")
-        self.label7.setFixedWidth(100)
-        label_layout.addWidget(self.label7)
-        # self.comboBox7 = QComboBox()
-        # self.comboBox7.setFixedWidth(100)
-        # self.comboBox7.addItem("选项1", "值1")
-        # self.comboBox7.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox7)
-
-
-        self.label8 = QLabel("州")
-        self.label8.setFixedWidth(100)
-        label_layout.addWidget(self.label8)
-        # self.comboBox8 = QComboBox()
-        # self.comboBox8.setFixedWidth(100)
-        # self.comboBox8.addItem("选项1", "值1")
-        # self.comboBox8.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox8)
-
-        self.label9 = QLabel("国家地区")
-        self.label9.setFixedWidth(100)
-        label_layout.addWidget(self.label9)
-        # self.comboBox9 = QComboBox()
-        # self.comboBox9.setFixedWidth(100)
-        # self.comboBox9.addItem("选项1", "值1")
-        # self.comboBox9.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox9)
-
-        self.label10 = QLabel("电话号码")
-        self.label10.setFixedWidth(100)
-        label_layout.addWidget(self.label10)
-        # self.comboBox10 = QComboBox()
-        # self.comboBox10.setFixedWidth(100)
-        # self.comboBox10.addItem("选项1", "值1")
-        # self.comboBox10.addItem("选项2", "值2")
-        # form_layout.addWidget(self.comboBox10)
+        self.comboBox1.setStyleSheet("""QComboBox {
+            border: 1px solid lightgray;
+            background-color: black;
+            padding: 4px; /* 增加内边距 */
+            font-size: 14pt; /* 增加字体大小 */
+        }
+        QComboBox::drop-down {
+            subcontrol-origin: padding;
+            subcontrol-position: right center;
+            width: 15px;
+            height: 15px;
+        }
+        QComboBox QAbstractItemView {
+            border: 0;
+            background-color: black; /* 下拉列表背景色 */
+            padding: 2px; /* 下拉列表内部的内边距 */
+        }
+        QComboBox QAbstractItemView::item {
+            border: 0;
+            padding: 4px 2px; /* 增加条目的内边距 */
+            min-height: 24px; /* 设置最小高度 */
+        }
+        """)
 
         widget = QWidget()
         widget.setLayout(main_layout)
@@ -230,60 +155,46 @@ class MainWindow(QMainWindow):
 
     def on_click_button1(self):
         text, ok = QInputDialog.getMultiLineText(
-            self, "导入账号密码", "请输入邮箱和密码(每行一个，用空格分隔)"
+            self, "导入账号,密码,超链接", "请输入账号,密码,超链接(每行一个，用空格分隔)"
         )
         if ok and text:
             lines = text.split("\n")
             for line in lines:
                 line = line.strip()
                 if line:
-                    email, password, *rest = line.split(maxsplit=1)
-                    if rest:
-                        print(f"警告：格式错误 - 多余的空格被忽略。行内容：{line}")
-                    self.add_email_and_password_to_table(email, password)
+                    email, password, url = line.split()
+                    self.add_email_password_and_url_to_table(email, password, url)
 
-    def add_email_and_password_to_table(self, email, password):
+    def add_email_password_and_url_to_table(self, email, password, url):
         row_position = self.tableWidget.rowCount()
         self.tableWidget.insertRow(row_position)
         email_item = QTableWidgetItem(email)
         password_item = QTableWidgetItem(password)
+        url_item = QTableWidgetItem(url)
         self.tableWidget.setItem(row_position, 0, email_item)
         self.tableWidget.setItem(row_position, 1, password_item)
+        self.tableWidget.setItem(row_position, 2, url_item)
 
     def on_click_button2(self):
         for row in range(self.tableWidget.rowCount()):
             if self.tableWidget.item(row, 0) and self.tableWidget.item(row, 1):
                 email = self.tableWidget.item(row, 0).text()
-                password = self.tableWidget.item(row, 1).text()
-                address_details = {
-                    "name":email,
-                    "pwd":password,
-                    "fullDaytimePhone": self.comboBox1.currentText(),#7
-                    "street2": self.comboBox2.currentText(),
-                    "lastName": self.comboBox3.currentText(),#1
-                    "firstName": self.comboBox4.currentText(),#2
-                    "companyName": self.comboBox5.currentText(),
-                    "street": self.comboBox6.currentText(),#3
-                    "city": self.comboBox7.currentText(),#5
-                    "state": self.comboBox8.currentText(),#6
-                    "postalCode": self.comboBox9.currentText(),#4
-                    "countryCode": self.comboBox10.currentText(),
+                selected_text = self.comboBox1.currentText()
+                parts = selected_text.split('----')
+                params = {
+                    "name":self.tableWidget.item(row, 0).text(),
+                    "pwd":self.tableWidget.item(row, 1).text(),
+                    "url": self.tableWidget.item(row, 2).text(),
+                    "city": parts[2].strip(),#城市
+                    "state": parts[3].strip(),#州
+                    "lastName": parts[0].strip(),#姓
+                    "firstName": parts[1].strip(),#名
+                    "companyName": '',#"公司名称"
+                    "street": parts[4].strip(),#"街道"
+                    "postalCode": parts[6].strip(),#"邮政编码"
+                    "street2": parts[5].strip(),#"街道2"
                 }
-                # params = {
-                #     "name": email,
-                #     "pwd": password,
-                #     "fullDaytimePhone": "111111-2222",
-                #     "street2": "212231312",
-                #     "lastName": "lllll3333",
-                #     "firstName": "hhhjjj11166666",
-                #     "companyName": "122332",
-                #     "street": "777776667777",
-                #     "city": "Albany",
-                #     "state": "CH",
-                #     "postalCode": "11111-1111",
-                #     "countryCode": "CH",
-                # }
-                params = {**params}
+                # params = {**params}
                 task = Asynctask(row, email, self.update_table_item, **params)
                 self.thread_pool.start(task)
 
@@ -292,36 +203,15 @@ class MainWindow(QMainWindow):
             self.tableWidget.setItem(row, 2, QTableWidgetItem(result["firstName"]))
         else:
             if self.tableWidget.item(row, 0).text() == email:
-                self.tableWidget.setItem(
-                    row, 2, QTableWidgetItem(result.get("firstName", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 3, QTableWidgetItem(result.get("lastName", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 4, QTableWidgetItem(result.get("companyName", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 5, QTableWidgetItem(result.get("street", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 6, QTableWidgetItem(result.get("street2", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 7, QTableWidgetItem(result.get("postalCode", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 8, QTableWidgetItem(result.get("city", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 9, QTableWidgetItem(result.get("state", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 10, QTableWidgetItem(result.get("countryCode", ""))
-                )
-                self.tableWidget.setItem(
-                    row, 11, QTableWidgetItem(result.get("fullDaytimePhone", ""))
-                )
+                self.tableWidget.setItem(row, 3, QTableWidgetItem(result.get("city", "")))
+                self.tableWidget.setItem(row, 4, QTableWidgetItem(result.get("state", "")))
+                self.tableWidget.setItem(row, 5, QTableWidgetItem(result.get("lastName", "")))
+                self.tableWidget.setItem(row, 6, QTableWidgetItem(result.get("firstName", "")))
+                self.tableWidget.setItem(row, 7, QTableWidgetItem(result.get("companyName", "")))
+                self.tableWidget.setItem(row, 8, QTableWidgetItem(result.get("street", "")))
+                self.tableWidget.setItem(row, 9, QTableWidgetItem(result.get("postalCode", "")))
+                self.tableWidget.setItem(row, 10, QTableWidgetItem(result.get("street2", "")))
+               
 
     def on_click_button3(self):
         desktop_path = str(Path.home() / "Desktop")
